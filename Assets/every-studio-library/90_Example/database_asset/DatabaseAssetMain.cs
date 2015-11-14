0b3381209caeb57f59c6e903a00400f6e1fc66c4
mainitiamai <mainitiamai@gmail.com>
2015-11-14 21:34:46 +0900

    データベースっぽいものを作ろうと思った

---
+++ Assets/every-studio-library/90_Example/database_asset/DatabaseAssetMain.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DatabaseAssetMain : MonoBehaviour {
+
+	//Singleton使用時はコンストラクタはつぶしておく
+	private EditPlayerSettingsData m_EditPlayerSettingsData;
+
+	public EditPlayerSettingsData GetEditPlayerSettingsData ()
+	{
+		if (m_EditPlayerSettingsData == null) {
+			Debug.Log ("no EditPlayserSettingsData");
+			UnityEngine.Object player_settings_assets;
+			player_settings_assets = Resources.Load ("EditPlayerSettingsData");
+			m_EditPlayerSettingsData = (EditPlayerSettingsData)player_settings_assets;
+		}
+		return m_EditPlayerSettingsData;
+	}
+
+	public DataSample m_DataSample;
+
+
+	// Use this for initialization
+	void Start () {
+
+		UnityEngine.Object player_settings_assets;
+		player_settings_assets = Resources.Load ("ExampleAsset");
+		m_DataSample = (DataSample)player_settings_assets;
+
+		Debug.Log (m_DataSample.moji);
+		Debug.Log (m_DataSample.param);
+
+		m_DataSample.moji = "chang";
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/every-studio-library/90_Example/database_asset/EditorConfig/DataSample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class DataSample : ScriptableObject {
+
+	public string moji;
+	public int param = 10;
+	public List<int> int_list = new List<int>();
+
+	public bool toggle;
+
+}
